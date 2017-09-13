75c49a2b6cd257d08efdb126ddcb68608f34d044
nickgorab <gorabn6@students.rowan.edu>
2017-09-13 17:21:38 -0400

    Blanket add to make sure I'm not forgetting to update anything

--- Simple Blink/MSP430F5529/main.c
+++ Simple Blink/MSP430F5529/main.c
@@ -1,17 +1,17 @@
-#include <msp430f5529.h>	// Includes the header file for the board
+#include <msp430f5529.h>    // Includes the header file for the board
 
 int main(void){
 
-	volatile int i;
+    volatile int i;
 
-	WDTCTL = WDTPW+WDTHOLD; 	// Stops the watchdog timer
+    WDTCTL = WDTPW+WDTHOLD;     // Stops the watchdog timer
 
-	P1DIR = BIT6;				// Sets pin 6 as an output
+    P1DIR = BIT0;               // Sets pin 1.0 as an output, this is the red LED
 
-	P1OUT = BIT6;				// Sets pin 6 as high
+    P1OUT = BIT0;               // Sets pin 1.0 as high, turning the LED on
 
-	while(1) {					// Creates a loop that toggles the LED
-		P1OUT ^=(BIT6)			// Section of code that toggles the LED
-		for(i=0; i<9999; i++)	//
-	}
+    while(1) {                  // Creates a loop that toggles the LEDs
+        P1OUT ^=(BIT0);     
+             
+        for(i=0; i<20000; i++);   // Adds a delay for the flashing
 }
