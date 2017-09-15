2eab040d3c83793f0720b722a84b35d93b468125
nickgorab <gorabn6@students.rowan.edu>
2017-09-15 15:44:38 -0400

    Added code for the FR6989

---
+++ Button Blink/MSP430FR6989/buttonToggle.c
@@ -0,0 +1,24 @@
+#include <msp430.h>
+
+ void main(void)
+ {
+
+  WDTCTL = WDTPW|WDTHOLD;   // Disables Watchdog timer
+
+  P1DIR = BIT0;             // Established the LED as an output
+  P1REN = BIT1;             // Enables the resistor for the button
+  P1OUT = BIT1;             // Creates a pull-up resistor
+  
+  while(1){                 // Creates an infinite loop 
+
+    if((P1IN & BIT1) == 0x00){          // If Pin_1 is an input and the button is pressed
+    __delay_cycles(5000);               // Bouncing protection via software delay
+
+      if((P1IN & BIT1) == 0x00){        // If Pin_1 is an input and the button is pressed
+        P1OUT ^= BIT0;                  // ORs the register location with a 1, which will toggle the element
+
+        while((P1IN & BIT1) == 0x00);   // While the pin and button are pressed keep the LED turned on, helps with bouncing
+      }
+    }
+  }
+}
---
+++ Multiple Blink/MSP430FR6989/multipleBlink.c
@@ -0,0 +1,26 @@
+#include <msp430f5529.h>    // Includes the header file for the board
+
+int main(void){
+
+    volatile int i;
+
+    WDTCTL = WDTPW+WDTHOLD;     // Stops the watchdog timer
+
+    P1DIR = BIT0;               // Sets pin 1.0 as an output
+    P9DIR = BIT7;               // Sets pin 4.7 as an output
+
+    P1OUT = BIT0;               // Sets pin 1.0 as high
+    P9OUT = BIT7;               // Sets pin 4.7 as high
+
+    while(1) {                  // Creates a loop that toggles the LED
+        P1OUT ^=(BIT0);          // Section of code that toggles the LED
+
+
+        for(i=0; i<5000; i++) {
+            if(i % 1000 == 0) {
+                P9OUT ^=(BIT7);
+            }
+        }
+    }
+}
+
